bcc3e16d98d6cfb4b021f8b13e44a2155fd7776a
Anchoriz <bjanchoriz22@gmail.com>
2025-06-24 13:42:57 +0800

    Added API

---
+++ WebApplication2/Controllers/LoadAccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LoadCommon;
+using Load_BusinessDataLogic;
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoadAccountController : ControllerBase
+    {
+        LoadBusinessService loadBusinessService = new LoadBusinessService();
+        [HttpGet("Get All Accounts")]
+        public IEnumerable<LoadAccount> GetData()
+        {
+            var accounts = loadBusinessService.GetAccounts();
+            return accounts;
+        }
+
+        [HttpPost("Add Account")]
+        public void AddLoadAccount(string phoneNum, string name, string PIN)
+        {
+            loadBusinessService.AddUserAccount(phoneNum, name, PIN);
+        }
+
+        [HttpPatch("Cash In")]
+        public bool CashIn(double amount, string phoneNum, string pin)
+        {
+            var result = loadBusinessService.UpdateBalance(1, amount, phoneNum, pin);
+
+            return result;
+        }
+
+        [HttpPatch("Send Cash")]
+        public bool SendCash(double amount, string phoneNum, string pin)
+        {
+            var result = loadBusinessService.UpdateBalance(2, amount, phoneNum, pin);
+
+            return result;
+        }
+
+        [HttpPatch("Change Username")]
+        public void ChangeName(string phoneNumber, string pin, string newName)
+        {
+            loadBusinessService.ChangeName(phoneNumber, pin, newName);
+        }
+
+        [HttpPatch("Change PIN")]
+        public void ChangePIN(string phoneNumber, string pin, string newPIN)
+        {
+            loadBusinessService.ChangePIN(phoneNumber, pin, newPIN);
+        }
+    }
+}
+
